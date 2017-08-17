666e1d55e3f20c4f60dace5d6c623c86642bf648
Daniel Waggoner <daxxon37@hotmail.com>
2017-08-17 10:32:21 -0400

    removed commented console.logs

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const path = require('path');
 const mustacheExpress = require('mustache-express');
 const express = require('express');
 const app = express();
+const morgan = require("morgan");
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
-app.set('view engine', 'mustache')
+app.set('view engine', 'mustache');
 
-app.use(express.static("public"))
-// app.use('/css', express.static("public"));
-// app.use(express.static('/'))
-// app.use('/user', express.static(path.join(__dirname, 'public')))
+app.use(morgan("combined"));
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
   res.render('index', data);
@@ -21,12 +20,6 @@ app.get('/user/:id', (req, res) => {
   let myIndex = req.params.id - 1;
   let user = data.users[myIndex];
   res.render('user', user);
-  console.log("My index: " + myIndex);
-  console.log("My user id: " + user.id);
-  console.log(data.users[myIndex]);
 })
 
-app.listen(3000, () => {
-  console.log('SUCCESSFUL TRANSMISSION');
-  // console.log(data);
-});
+app.listen(3000, () => console.log('SHOW ME WHAT YOU GOT'));
